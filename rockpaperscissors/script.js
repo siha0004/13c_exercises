1cd1ab9fa4ff5f534b390389e5efbd0a434c455a
Sif Melkane <siha0004@stud.kea.dk>
2024-09-17 20:35:17 +0200

    Adding comments

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -1,10 +1,10 @@
 "use strict";
 
+// KONSTANTER
 const rock = document.querySelector(".rock");
 const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
-// const players = document.querySelector(".player");
 const player1 = document.querySelector("#player1");
 const player2 = document.querySelector("#player2");
 
@@ -12,15 +12,18 @@ const lose = document.querySelector("#lose");
 const win = document.querySelector("#win");
 const draw = document.querySelector("#draw");
 
+// VARIABLER
 let userGuess;
 let computerGuess;
 let randomNum;
 let result;
 
+// Eventlisteners til knap-klik
 rock.addEventListener("click", rockClicked);
 paper.addEventListener("click", paperClicked);
 scissors.addEventListener("click", scissorsClicked);
 
+// Funktioner som ændre userGuess variable, alt efter hvad der er klikket på
 function rockClicked() {
   console.log("rockClicked");
   userGuess = "rock";
@@ -40,25 +43,30 @@ function scissorsClicked() {
   prepGame();
 }
 
+// Funktion som forbereder spillet
 function prepGame() {
+  // Nulsstiller alle classes
   player1.classList = "";
   player2.classList = "";
   draw.classList = "";
   win.classList = "";
   lose.classList = "";
 
+  // Sætter 'grund'-classes på igen
   player1.classList.add("player");
   player2.classList.add("player");
   draw.classList.add("hidden");
   win.classList.add("hidden");
   lose.classList.add("hidden");
 
+  // Genere et tilfældigt tal mellem 1-3
   randomNum = Math.floor(Math.random() * 3) + 1;
   console.log("Random number is: ", randomNum);
 
   computerGuesses();
 }
 
+// Definere computerguess, ud fra det tilfældige tal
 function computerGuesses() {
   if (randomNum === 1) {
     computerGuess = "rock";
@@ -73,13 +81,16 @@ function computerGuesses() {
   console.log("ComputerGuess is: ", computerGuess);
   console.log("Userguess is: ", userGuess);
 
+  // starter shake-animationer
   player1.classList.add("shake");
   player2.classList.add("shake");
 
+  // Når animationen er slut, fortsættes til næste funktion
   player1.addEventListener("animationend", player1Hand);
   player2.addEventListener("animationend", player2Hand);
 }
 
+// Ændre hånd-illustration alt efter hvad BRUGEREN har klikket på
 function player1Hand() {
   if (userGuess === "paper") {
     player1.classList.add("paper");
@@ -90,6 +101,8 @@ function player1Hand() {
   }
   whoWins();
 }
+
+// Ændre hånd-illustration alt efter hvad COMPUTEREN har valgt (tilfældigt tal)
 function player2Hand() {
   if (computerGuess === "paper") {
     player2.classList.add("paper");
@@ -101,6 +114,7 @@ function player2Hand() {
   whoWins();
 }
 
+// Tjekke om brugeren har vunder, tabt eller om det er uafgjort
 function whoWins() {
   if (userGuess === computerGuess) {
     result = "draw";
@@ -121,6 +135,7 @@ function whoWins() {
   resetGame();
 }
 
+//Fjerne shake-class - virker ikke hvis jeg fjerne dette, forstår ikke helt hvorfor, når jeg ellers har prepGame()...
 function resetGame() {
   console.log("resetGame");
 
